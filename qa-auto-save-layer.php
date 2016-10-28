c32f53dccd49e8416957b4a7d3e9bbb9b71e181b
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-10-28 15:37:29 +0900

    条件にエディター名追加

--- qa-auto-save-layer.php
+++ qa-auto-save-layer.php
@@ -6,8 +6,10 @@ class qa_html_theme_layer extends qa_html_theme_base
     {
         
         if ($this->template === 'ask') {
-            $script = QA_HTML_THEME_LAYER_URLTOROOT.'js/autosave.js';
-            $this->output('<script src="'.$script.'"></script>');
+            if (qa_opt('editor_for_as') === 'Medium Editor') {
+                $script = QA_HTML_THEME_LAYER_URLTOROOT.'js/autosave-medium.js';
+                $this->output('<script src="'.$script.'"></script>');
+            }
         }
         qa_html_theme_base::body_footer();
     }
