3a235c138ad0c3fd4d7eb639cc7e98f60021b772
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-10-28 11:35:57 +0900

    Plugin ファイル追加

---
+++ qa-plugin.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+	Plugin Name: Auto Save
+	Plugin URI: Provide an automatic save.
+	Plugin Description: Test Plugin
+	Plugin Version: 1.0
+	Plugin Date: 2016-10-28
+	Plugin Author: 38qa.net
+	Plugin Author URI: http://38qa.net/
+	Plugin License: GPLv2
+	Plugin Minimum Question2Answer Version: 1.7
+	Plugin Update Check URI:
+*/
+
+if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
+	header('Location: ../../');
+	exit;
+}
