35d8264da6cc76efc311869144b056d94bd452ac
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-10-31 16:44:26 +0900

    データの保存と読み込み実装

--- js/autosave-medium.js
+++ js/autosave-medium.js
@@ -1,7 +1,13 @@
 $(function($) {
+    var is_first = true;
     var is_autosave_start = false;
     var timer_id;
-
+    var editor_index = 0;
+    
+    if (is_first) {
+        ajax_get_item();
+    }
+    
     $('#title').keyup(function(){
         if (!is_autosave_start) {
             autosave_start();
@@ -11,32 +17,55 @@ $(function($) {
     $('.editable').keyup(function(){
         if (!is_autosave_start) {
             autosave_start();
+            editor_index = $(this).data('medium-editor-editor-index') - 1;
         }
     });
     
     function autosave_start() {
-        console.log('auto save start!');
         is_autosave_start = true;
-        timer_id = setInterval(ajax_set_item, 10000);
+        timer_id = setInterval(ajax_set_item, 15000);
     }
     
     function ajax_set_item () {
-        // jQuery.post(ajax_url, { field: field, value: value }, function(response) {
-        //     console.log(response);
-        // });
-        console.log('call ajax set item');
+        var JSONdata = {
+          index: editor_index,
+          title: $('#title').val(),
+          content: get_content()
+        }; 
+        $.ajax({
+            url: ajax_url + resource,
+            type: 'POST',
+            dataType: 'json',
+            cache : false,
+            data: JSON.stringify(JSONdata)
+        })
+        .done(function(res) {
+            console.log('Auto Save');
+        })
+        .fail(function(res) {
+            console.log(res[0]);
+        });
     }
+    
     function ajax_get_item () {
-        // $.ajax({
-        //     url: ajax_url,
-        //     type: 'GET',
-        //     dataType: 'json',
-        //     cache : false,
-        //     data: {data}
-        // })
-        // .done(function(res) {
-        //     console.log('Get Item');
-        // });
+        $.ajax({
+            url: ajax_url + resource,
+            type: 'GET',
+            dataType: 'json',
+            cache : false,
+            data: {}
+        })
+        .done(function(res) {
+            console.log('Get Item');
+            if (res[0] !== null && res[0] !== undefined) {
+                if ($('title') !== undefined ) {
+                    $('#title').val(res[0].title);
+                }
+                editor.setContent(res[0].content, res[0].index);
+            }
+        })
+        .fail(function(res) {
+            console.log(res[0]);
+        });
     }
-
 });
--- qa-auto-save-layer.php
+++ qa-auto-save-layer.php
@@ -4,9 +4,11 @@ class qa_html_theme_layer extends qa_html_theme_base
 {
     function body_footer()
     {
-        
         if ($this->template === 'ask') {
-            if (qa_opt('editor_for_as') === 'Medium Editor') {
+            if (qa_opt('editor_for_qs') == 'Medium Editor') {
+                $script = 'var ajax_url = "'.qa_path('autosave').'/"; ';
+                $script .= 'var resource = "question";';
+                $this->output('<script>'.$script.'</script>');
                 $script = QA_HTML_THEME_LAYER_URLTOROOT.'js/autosave-medium.js';
                 $this->output('<script src="'.$script.'"></script>');
             }
