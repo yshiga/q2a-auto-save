9df2910872496580f0938f5e2c62bc5a3b1c7566
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-10-31 16:43:55 +0900

    ページモジュール読み込み

--- qa-plugin.php
+++ qa-plugin.php
@@ -20,3 +20,5 @@ if (!defined('QA_VERSION')) { // don't allow this page to be requested directly 
 
 // layer
 qa_register_plugin_layer('qa-auto-save-layer.php','Auto Save Layer');
+// page
+qa_register_plugin_module('page', 'qa-auto-save-response.php', 'qa_auto_save_response_page', 'Auto Save Page');
