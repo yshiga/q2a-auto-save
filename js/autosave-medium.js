b4345e7d9facbb42e5221a6e81afe72282e5dac8
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-11-01 10:15:24 +0900

    エディタの内容から動画関連のタグをとりのぞかない
    投稿ボタンクリック時にも保存
    
    タイトルや内容に何も書かれていないときだけ保存してあるものを反映する

--- js/autosave-medium.js
+++ js/autosave-medium.js
@@ -1,10 +1,12 @@
 $(function($) {
-    var is_first = true;
     var is_autosave_start = false;
     var timer_id;
     var editor_index = 0;
     
-    if (is_first) {
+    var title = $('#title') ? $('#title').val() : '';
+    var content = editor_content();
+    content = content.replace(/(<([^>]+)>)/ig,"");
+    if (title === '' && content === '') {
         ajax_get_item();
     }
     
@@ -17,20 +19,31 @@ $(function($) {
     $('.editable').keyup(function(){
         if (!is_autosave_start) {
             autosave_start();
-            editor_index = $(this).data('medium-editor-editor-index') - 1;
         }
     });
+    
+    $('#q_submit').click(function(){
+        ajax_set_item();
+    })
     
     function autosave_start() {
         is_autosave_start = true;
         timer_id = setInterval(ajax_set_item, 30000);
     }
     
+    function editor_content() {
+        var allContents = editor.serialize();
+        var editorId = editor.elements[0].id;
+        var content = allContents[editorId].value;
+        return content;
+    }
+    
     function ajax_set_item () {
+        editor_index = $('.editable').data('medium-editor-editor-index') - 1;
         var JSONdata = {
           index: editor_index,
           title: $('#title').val(),
-          content: get_content()
+          content: editor_content()
         }; 
         $.ajax({
             url: ajax_url + resource,
@@ -46,7 +59,7 @@ $(function($) {
             console.log(res[0]);
         });
     }
-    
+        
     function ajax_get_item () {
         $.ajax({
             url: ajax_url + resource,
@@ -58,7 +71,7 @@ $(function($) {
         .done(function(res) {
             console.log('Get Item');
             if (res[0] !== null && res[0] !== undefined) {
-                if ($('title') !== undefined ) {
+                if ($('title') !== undefined) {
                     $('#title').val(res[0].title);
                 }
                 editor.setContent(res[0].content, res[0].index);
