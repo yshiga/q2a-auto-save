e9795457acdae7a87bbdaa029ab772b6a09214b6
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-11-07 18:29:53 +0900

    質問詳細ページでもautosave用スクリプト出力

--- qa-auto-save-layer.php
+++ qa-auto-save-layer.php
@@ -14,15 +14,24 @@ class qa_html_theme_layer extends qa_html_theme_base
     
     function body_footer()
     {
-        if ($this->template === 'ask') {
-            if (qa_opt('editor_for_qs') == 'Medium Editor') {
-                $script = 'var ajax_url = "'.qa_path('autosave').'/"; ';
-                $script .= 'var resource = "question";';
-                $this->output('<script>'.$script.'</script>');
-                $script = QA_HTML_THEME_LAYER_URLTOROOT.'js/autosave-medium.js';
-                $this->output('<script src="'.$script.'"></script>');
-            }
+        $ajax = 'var ajax_url = "'.qa_path('autosave').'/"; ';
+        if ($this->template === 'ask'
+            && qa_opt('editor_for_qs') === 'Medium Editor') {
+            $ajax .= 'var resource = "question";';
+            $script = QA_HTML_THEME_LAYER_URLTOROOT.'js/autosave-medium.js';
+            $this->output_script($ajax, $script);
+        } elseif ($this->template === 'question'
+                  && qa_opt('editor_for_as') == 'Medium Editor') {
+            $ajax .= 'var resource = "answer";';
+            $script = QA_HTML_THEME_LAYER_URLTOROOT.'js/autosave-medium.js';
+            $this->output_script($ajax, $script);
         }
         qa_html_theme_base::body_footer();
+    }
+    
+    private function output_script($ajax, $script)
+    {
+        $this->output('<script>'.$ajax.'</script>');
+        $this->output('<script src="'.$script.'"></script>');
     }
 }
