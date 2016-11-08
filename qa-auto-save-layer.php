455e2f3dfd6123237f5c49364c62aa49bc900636
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-11-08 15:37:20 +0900

    post_idをjsで使用できるようにする

--- qa-auto-save-layer.php
+++ qa-auto-save-layer.php
@@ -14,15 +14,18 @@ class qa_html_theme_layer extends qa_html_theme_base
     
     function body_footer()
     {
-        $ajax = 'var ajax_url = "'.qa_path('autosave').'/"; ';
+        $ajax = 'var ajax_url = "'.qa_path('autosave').'/";';
         if ($this->template === 'ask'
             && qa_opt('editor_for_qs') === 'Medium Editor') {
             $ajax .= 'var resource = "question";';
+            $ajax .= 'var post_id = "0000";';
             $script = QA_HTML_THEME_LAYER_URLTOROOT.'js/autosave-medium.js';
             $this->output_script($ajax, $script);
         } elseif ($this->template === 'question'
                   && qa_opt('editor_for_as') == 'Medium Editor') {
+            $post_id = $this->get_post_id();
             $ajax .= 'var resource = "answer";';
+            $ajax .= 'var post_id ="'.$post_id.'";';
             $script = QA_HTML_THEME_LAYER_URLTOROOT.'js/autosave-medium.js';
             $this->output_script($ajax, $script);
         }
@@ -33,5 +36,14 @@ class qa_html_theme_layer extends qa_html_theme_base
     {
         $this->output('<script>'.$ajax.'</script>');
         $this->output('<script src="'.$script.'"></script>');
+    }
+    
+    private function get_post_id()
+    {
+        $post_id = qa_request_part(0);
+        if (empty($post_id) || !is_numeric($post_id)) {
+            $post_id = "0000";
+        }
+        return $post_id;
     }
 }
